8a7aa33a39db4edc8af66cdb59d0ace5cd646433
Ashwani-cool <53433767+Ashwani-cool@users.noreply.github.com>
2024-02-15 19:11:04 +0530

    Add files via upload

---
+++ ExpenseTracker/src/App.tsx
@@ -0,0 +1,10 @@
+import ExpenseTrackerApp from './Components/ExpenseTrackerHome';
+import "bootstrap/dist/css/bootstrap.min.css"
+
+export default function App() {
+  return (
+    <div className="App">
+      {<ExpenseTrackerApp></ExpenseTrackerApp>}
+    </div>
+  )
+}
